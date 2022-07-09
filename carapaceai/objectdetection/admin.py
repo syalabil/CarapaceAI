a7f60753e1a4120648f4c429498e4a71471ba34f
Kevan Soon <19013989@myrp.edu.sg>
2022-07-09 17:14:26 +0800

    Create Django Super User and added Static Files

--- carapaceai/objectdetection/admin.py
+++ carapaceai/objectdetection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class HistoryAdmin(admin.ModelAdmin):
+    model = History
+    list_display = ['id', 'result_video','tested_on',"passed"]
+
+admin.site.register(History,HistoryAdmin)
+admin.site.register(Video)
