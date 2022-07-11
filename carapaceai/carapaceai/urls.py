f2e53e22689758bed3c5e514681fc280367be42f
Kevan Soon <19013989@myrp.edu.sg>
2022-07-11 15:05:11 +0800

    testing branch

--- carapaceai/carapaceai/urls.py
+++ carapaceai/carapaceai/urls.py
@@ -1,4 +1,4 @@
-"""carapaceai URL Configuration
+"""carapaceAI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('objectdetection.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- carapaceai/carapaceai/wsgi.py
+++ carapaceai/carapaceai/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for carapaceai project.
+WSGI config for carapaceAI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carapaceai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carapaceAI.settings')
 
 application = get_wsgi_application()
